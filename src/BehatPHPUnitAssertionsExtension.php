40a6207459046c69a65e50ba7627d6a7216c40d0
acoulton <andrew@ingenerator.com>
2026-09-16 16:48:18 +0100

    feat: Add support for PHPUnit 8, 9, and 10
    Add separate Behat profiles for each supported version - we can then run
    the correct profile in CI based on the phpunit version installed.
    
    Note that the PHPUnitExceptionStringer implementation is exactly the one
    copied from Behat itself to start with.

--- src/BehatPHPUnitAssertionsExtension.php
+++ src/BehatPHPUnitAssertionsExtension.php
@@ -2,10 +2,13 @@
 
 namespace Behat\PHPUnitAssertionsExtension;
 
+use Behat\Testwork\Exception\ServiceContainer\ExceptionExtension;
+use Behat\Testwork\Exception\Stringer\PHPUnitExceptionStringer;
 use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 final class BehatPHPUnitAssertionsExtension implements Extension
 {
@@ -16,21 +19,23 @@ final class BehatPHPUnitAssertionsExtension implements Extension
 
     public function initialize(ExtensionManager $extensionManager): void
     {
-        // TODO: Implement initialize() method.
+        // No-op
     }
 
     public function configure(ArrayNodeDefinition $builder): void
     {
-        // TODO: Implement configure() method.
+        // No-op
     }
 
     public function load(ContainerBuilder $container, array $config): void
     {
-        // TODO: Implement load() method.
+        $definition = new Definition(PHPUnitExceptionStringer::class);
+        $definition->addTag(ExceptionExtension::STRINGER_TAG, ['priority' => 50]);
+        $container->setDefinition(ExceptionExtension::STRINGER_TAG. '.phpunit_assertions', $definition);
     }
 
     public function process(ContainerBuilder $container): void
     {
-        // TODO: Implement process() method.
+        // No-op
     }
 }
